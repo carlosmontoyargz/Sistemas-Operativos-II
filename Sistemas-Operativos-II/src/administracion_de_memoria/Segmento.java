7940461d47b46fc5359706f620b8577c9e6c655d
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-05-29 18:02:45 -0500

    Update Segmento.java

--- Sistemas-Operativos-II/src/administracion_de_memoria/Segmento.java
+++ Sistemas-Operativos-II/src/administracion_de_memoria/Segmento.java
@@ -6,28 +6,5 @@ package administracion_de_memoria;
  */
 public class Segmento
 {
-    private final String Nombre;
-	private final int Direccion;
-	private final int Longitud;
-	private int Tiempo;
-	
-	public Segmento(String nombre, int direccion, int longitud, int tiempo)
-	{
-		this.Nombre = nombre;
-		this.Direccion = direccion;
-		this.Longitud = longitud;
-		this.Tiempo = tiempo;
-	}
-	
-	public void decrementarTiempo()
-	{
-		this.Tiempo--;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return "[" + this.Nombre + ", " + this.Direccion + ", " + this.Longitud +
-				", " + this.Tiempo + "]";
-	}
+
 }
