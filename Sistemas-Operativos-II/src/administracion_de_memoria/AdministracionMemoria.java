5b135359382678157b19af2ed18b6cb8d730bab9
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-05-31 01:52:45 -0500

    Update Segmento.java

---
+++ Sistemas-Operativos-II/src/administracion_de_memoria/AdministracionMemoria.java
@@ -0,0 +1,13 @@
+package administracion_de_memoria;
+
+/**
+ *
+ * @author carlosmontoya
+ */
+public class AdministracionMemoria
+{
+    public static void main(String[] args)
+	{	
+		System.out.println(new ListaSegmentos(1024));
+	}
+}
