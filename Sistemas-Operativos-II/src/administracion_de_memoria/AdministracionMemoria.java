67156270bfba0ba46b36bd7786c396c672e853f6
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-02 17:09:03 -0500

    Creada lista de pruebas

--- Sistemas-Operativos-II/src/administracion_de_memoria/AdministracionMemoria.java
+++ Sistemas-Operativos-II/src/administracion_de_memoria/AdministracionMemoria.java
@@ -7,7 +7,23 @@ package administracion_de_memoria;
 public class AdministracionMemoria
 {
     public static void main(String[] args)
-	{	
-		System.out.println(new ListaSegmentos(1024));
+	{
+		ListaSegmentos lista = new ListaSegmentos(1024);
+		System.out.println(lista);
+		
+		lista.primerAjuste("Prueba1", 100);
+		System.out.println(lista);
+		
+		lista.primerAjuste("Prueba2", 200);
+		System.out.println(lista);
+		
+		lista.primerAjuste("Prueba3", 250);
+		System.out.println(lista);
+		
+		lista.primerAjuste("Prueba4", 200);
+		System.out.println(lista);
+		
+		lista.primerAjuste("Prueba5", 274);
+		System.out.println(lista);
 	}
 }
