c30a7b9c00f23f3570c1e3512f57e77e069c69bf
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-04 04:03:00 -0500

    Agregada descripcion de la clase

--- Sistemas-Operativos-II/src/administracion_de_memoria/ListaPrimerAjuste.java
+++ Sistemas-Operativos-II/src/administracion_de_memoria/ListaPrimerAjuste.java
@@ -1,6 +1,11 @@
 package administracion_de_memoria;
 
-
+/**
+ * Clase que permite administrar una lista de segmentos de memoria mediante el
+ * algoritmo de primer ajuste.
+ * 
+ * @author carlosmontoya
+ */
 public class ListaPrimerAjuste extends ListaSegmentos
 {
 	public ListaPrimerAjuste(int memoriaTotal)
