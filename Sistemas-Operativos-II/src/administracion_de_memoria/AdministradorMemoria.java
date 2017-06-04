2924b5007c1c077a30e46f8bca3a69515bc33da8
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-04 00:46:17 -0500

    Initial commit

---
+++ Sistemas-Operativos-II/src/administracion_de_memoria/AdministradorMemoria.java
@@ -0,0 +1,12 @@
+package administracion_de_memoria;
+
+/**
+ *
+ * @author carlosmontoya
+ */
+public interface AdministradorMemoria
+{
+	public int agregar(String nombre, int longitud);
+	
+	public int eliminar(String nombre);
+}
