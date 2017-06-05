2d134e21601339383358f8997240bf5d05d30293
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-05 02:01:04 -0500

    Actualizado metodo para buscar el hueco.

--- Sistemas-Operativos-II/src/administracion_de_memoria/ListaSiguienteAjuste.java
+++ Sistemas-Operativos-II/src/administracion_de_memoria/ListaSiguienteAjuste.java
@@ -35,40 +35,15 @@ public class ListaSiguienteAjuste extends ListaSegmentos
 		
 		return agregado;
 	}
-
+	
 	@Override
 	protected Segmento buscarHueco(int longitud)
 	{
-		Segmento segmento = buscarHueco(longitud, 0);
+		Segmento segmento = super.buscarHueco(longitud, actual, null);
 		if (segmento == null)
-		{
-			segmento = buscarHueco(longitud, 1);
-			
-			if (segmento == this.actual) return null;
-		}
+			segmento = super.buscarHueco(longitud, null, actual);
 		
-		this.actual = segmento;
-		
-		return segmento;
-	}
-		
-	private Segmento buscarHueco(int longitud, int tipoBusqueda)
-	{
-		Segmento segmento, final1;
-		if (tipoBusqueda == 0)
-		{
-			segmento = this.actual;
-			final1 = null;
-		}
-		else
-		{
-			segmento = super.getPrimero();
-			final1 = this.actual;
-		}
-		
-		while (segmento != final1 &&
-				!(segmento.isHueco() && segmento.getLongitud() >= longitud))
-			segmento = segmento.getSiguiente();
+		if (segmento != null) this.actual = segmento;
 		
 		return segmento;
 	}
