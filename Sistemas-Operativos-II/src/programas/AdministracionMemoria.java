5f00cc00b8a7221ef9bcb16fac59b8fefb16c9cf
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-04 20:58:26 -0500

    Actualizado metodo main

--- Sistemas-Operativos-II/src/programas/AdministracionMemoria.java
+++ Sistemas-Operativos-II/src/programas/AdministracionMemoria.java
@@ -10,37 +10,6 @@ public class AdministracionMemoria
 {
     public static void main(String[] args)
 	{
-		ListaSegmentos lista = new ListaSiguienteAjuste(1024);
-		System.out.println(lista);
 		
-		lista.agregar("Prueba1", 100);
-		System.out.println(lista);
-		
-		lista.agregar("Prueba2", 200);
-		System.out.println(lista);
-		
-		lista.agregar("Prueba3", 250);
-		System.out.println(lista);
-		
-		lista.agregar("Prueba4", 200);
-		System.out.println(lista);
-		
-		lista.agregar("Prueba5", 273);
-		System.out.println(lista);
-		
-		lista.eliminar("Prueba2");
-		System.out.println(lista);
-		
-		lista.eliminar("Prueba5");
-		System.out.println(lista);
-		
-		lista.eliminar("Prueba3");
-		System.out.println(lista);
-		
-		lista.eliminar("Prueba4");
-		System.out.println(lista);
-		
-		lista.eliminar("Prueba1");
-		System.out.println(lista);
 	}
 }
