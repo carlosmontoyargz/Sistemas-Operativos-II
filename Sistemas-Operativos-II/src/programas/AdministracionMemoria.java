d4eab38acc3553ed2bda8c859a6d93e622dd0c48
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-05 02:40:25 -0500

    Actualizado metodo de prueba.

--- Sistemas-Operativos-II/src/programas/AdministracionMemoria.java
+++ Sistemas-Operativos-II/src/programas/AdministracionMemoria.java
@@ -21,55 +21,32 @@ public class AdministracionMemoria
 		
 		System.out.println("-------------------------------- Siguiente ajuste --------------------------------");
 		pruebas(new ListaSiguienteAjuste(1024));
+		System.out.println();
+		
+		System.out.println("-------------------------------- Mejor ajuste --------------------------------");
+		pruebas(new ListaMejorAjuste(1024));
+		System.out.println();
 	}
 	
 	public void pruebas(AdministradorMemoria adm)
 	{
 		System.out.println(adm);
 		
-		adm.agregar("P1", 100);
-		System.out.println(adm);
-		
-		adm.agregar("P2", 200);
-		System.out.println(adm);
-		
-		adm.agregar("P3", 250);
-		System.out.println(adm);
-		
-		adm.agregar("P4", 200);
-		System.out.println(adm);
-		
-		adm.agregar("P5", 273);
-		System.out.println(adm);
-		
-		adm.eliminar("P2");
-		System.out.println(adm);
-		
-		adm.eliminar("P5");
-		System.out.println(adm);
-		
-		adm.agregar("P6", 150);
-		System.out.println(adm);
-		
-		adm.agregar("P7", 110);
-		System.out.println(adm);
-		
-		adm.agregar("P8", 250);
-		System.out.println(adm);
-		
-		adm.eliminar("P3");
-		System.out.println(adm);
-		
-		adm.eliminar("P4");
-		System.out.println(adm);
-		
-		adm.eliminar("P1");
-		System.out.println(adm);
-		
-		adm.eliminar("P7");
-		System.out.println(adm);
-		
-		adm.eliminar("P6");
-		System.out.println(adm);
+		adm.agregar("P1", 100); System.out.println(adm);
+		adm.agregar("P2", 200); System.out.println(adm);
+		adm.agregar("P3", 250); System.out.println(adm);
+		adm.agregar("P4", 200); System.out.println(adm);
+		adm.agregar("P5", 273); System.out.println(adm);
+		adm.eliminar("P2"); System.out.println(adm);
+		adm.eliminar("P5"); System.out.println(adm);
+		adm.eliminar("P1"); System.out.println(adm);
+		adm.agregar("P6", 150); System.out.println(adm);
+		adm.agregar("P7", 110); System.out.println(adm);
+		adm.agregar("P8", 250); System.out.println(adm);
+		adm.eliminar("P3"); System.out.println(adm);
+		adm.eliminar("P4"); System.out.println(adm);
+		adm.eliminar("P7"); System.out.println(adm);
+		adm.eliminar("P6"); System.out.println(adm);
+		adm.eliminar("P8"); System.out.println(adm);
 	}
 }
