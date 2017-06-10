6d4a17e932caa92c63bf60ffddd8d7f7997bf14f
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-09 19:04:42 -0500

    Agregada documentacion.

--- Sistemas-Operativos-II/src/administracion_de_memoria/AdministradorMemoria.java
+++ Sistemas-Operativos-II/src/administracion_de_memoria/AdministradorMemoria.java
@@ -1,12 +1,26 @@
 package administracion_de_memoria;
 
 /**
- *
+ * Interface para un administrador de memoria.
+ * 
  * @author carlosmontoya
  */
 public interface AdministradorMemoria
 {
+	/**
+	 * Asigna memoria a un proceso.
+	 * 
+	 * @param nombre  El nombre del proceso a guardar.
+	 * @param longitud  El tamano en bytes del proceso.
+	 * @return 
+	 */
 	public boolean agregar(String nombre, int longitud);
 	
+	/**
+	 * Elimina un proceso guardado memoria.
+	 * 
+	 * @param nombre  El nombre del proceso a eliminar
+	 * @return 
+	 */
 	public boolean eliminar(String nombre);
 }
