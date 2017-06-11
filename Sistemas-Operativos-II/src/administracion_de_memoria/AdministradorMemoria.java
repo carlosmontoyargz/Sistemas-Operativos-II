28766374a4059149a2aee726f7128a0cae4d9595
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-06-11 04:26:47 -0500

    Removidos metodos depreciados

--- Sistemas-Operativos-II/src/administracion_de_memoria/AdministradorMemoria.java
+++ Sistemas-Operativos-II/src/administracion_de_memoria/AdministradorMemoria.java
@@ -24,24 +24,3 @@ public interface AdministradorMemoria
 	 */
 	public boolean eliminar(String nombre);
 }
-
-/*
-// Agrega el proceso
-		long t1 = new Date().getTime();
-		boolean agregado = agregarProceso(nombre, longitud);
-		long t2 = new Date().getTime();
-		
-		//Actualiza estadisticas
-		if (!agregado) this.numProcesosRechazados ++;
-		this.tiempoEjecucionAgregacion += t2 - t1;
-		System.out.println(t2 + " "  + t1);
-		return agregado;
-
-
-System.out.println("Tiempo de ejecucion de agregacion: " +
-				adm.tiempoEjecucionAgregacion() + "\n" + 
-				"Tiempo de ejecucion de eliminacion: " +
-				adm.tiempoEjecucionEliminacion() + "\n" +
-				"Total de procesos rechazados: " +
-				adm.numProcesosRechazados() + "\n");
-*/
